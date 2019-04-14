9fd11b76248d6e3a237582e0cb82e5efa3edb2c2
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2019-04-14 12:22:00 +0300

    add swagger support

--- Startup.cs
+++ Startup.cs
@@ -6,65 +6,94 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace News.Api
 {
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // const string connectionString = "Server=localhost,1433; Database=newsdbdev; User=SA; Password=Password1234";
+            const string connectionStringAzureDb =
+                "Server=tcp:newsdbdev.database.windows.net,1433;Initial Catalog=newsdbdev;Persist Security Info=False;User ID=omrumbakitemiz;Password=y6qB4V2g34r97a8z;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            services.AddDbContext<NewsDbContext>(options => options.UseSqlServer(connectionStringAzureDb));
+
+            services.AddCors(options =>
+            {
+                // this defines a CORS policy called "default"
+                options.AddPolicy("default", policy =>
+                {
+                    policy
+                  .AllowAnyHeader()
+                  .AllowAnyMethod()
+                  .WithOrigins("http://localhost:8100", "http://localhost:4200", "capacitor://localhost",
+                      "http://localhost", "http://10.60.177.229:8100")
+                  .AllowCredentials();
+                });
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "News Api",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "Ömrüm Baki Temiz",
+                        Email = "omrumbakitemiz@icloud.com",
+                        Url = "https://github.com/omrumbakitemiz"
+                    },
+                    License = new License
+                    {
+                        Name = "MIT",
+                        Url = "https://github.com/omrumbakitemiz/News-Api"
+                    }
+                });
+            });
+
+            services.AddSignalR();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddMvc()
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                    options.SerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                    options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors("default");
+
+            app.UseSignalR(builder => builder.MapHub<NewsHub>("/newsHub"));
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "News API V1");
+            });
+
+            app.UseMvc();
+        }
     }
-
-    public IConfiguration Configuration { get; }
-
-    public void ConfigureServices(IServiceCollection services)
-    {
-      // const string connectionString = "Server=localhost,1433; Database=newsdbdev; User=SA; Password=Password1234";
-      const string connectionStringAzureDb =
-          "Server=tcp:newsdbdev.database.windows.net,1433;Initial Catalog=newsdbdev;Persist Security Info=False;User ID=omrumbakitemiz;Password=y6qB4V2g34r97a8z;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-      services.AddDbContext<NewsDbContext>(options => options.UseSqlServer(connectionStringAzureDb));
-
-      services.AddCors(options =>
-      {
-              // this defines a CORS policy called "default"
-              options.AddPolicy("default", policy =>
-              {
-            policy
-                      .AllowAnyHeader()
-                      .AllowAnyMethod()
-                      .WithOrigins("http://localhost:8100", "http://localhost:4200", "capacitor://localhost",
-                          "http://localhost", "http://10.60.177.229:8100")
-                      .AllowCredentials();
-          });
-      });
-
-      services.AddSignalR();
-
-      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-      services.AddMvc()
-          .AddJsonOptions(options =>
-          {
-            options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-            options.SerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
-            options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-            options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
-          });
-    }
-
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-
-      app.UseCors("default");
-
-      app.UseSignalR(builder => builder.MapHub<NewsHub>("/newsHub"));
-
-      app.UseMvc();
-    }
-  }
 }
